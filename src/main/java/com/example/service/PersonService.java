19875a3a095dfd6fcb18c0ccd11b5eb503e8ba49
Abdelilah Kouzih <116494217+AbdelilahKouzih@users.noreply.github.com>
2024-03-28 10:34:08 +0000

    Update PersonService.java 6

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -50,8 +50,8 @@ public class PersonService {
 		p = new Person();
 		p.setId("10000");
 		p.setAge(25);
-		p.setFirstName("abdoooooooooooooooooooooooooo");
-		p.setLastName("testJenjinsJob");
+		p.setFirstName("sadam 7ssinn al majidddddd");
+		p.setLastName("l9awmiya al 3arabiya ");
 		persons.add(p);
 		
 		
